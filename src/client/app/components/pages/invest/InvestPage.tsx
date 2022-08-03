8a5de71330a04712a04a4663a0c935ea9b92117a
Dmitriy Green <dmitriy.zelenyi96@gmail.com>
2022-08-03 09:42:57 +0300

    Complete Invest page layout

--- src/client/app/components/pages/invest/InvestPage.tsx
+++ src/client/app/components/pages/invest/InvestPage.tsx
@@ -1,13 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import s from './styles.module.scss';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import useDeals from '../../../hooks/useDeals';
+import useDeals from 'hooks/useDeals';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
+import Button from '@mui/material/Button';
 
 const InvestPage: FC = () => {
-  const { assetId = "" } = useRouter().query;
+  const { assetId = '' } = useRouter().query;
   const deal = useDeals({ id: assetId as string }).deals[0];
+  const [tokenQuantity, setTokenQuantity] = useState(1);
 
   if (!deal) {
     return <div>Id not found</div>;
@@ -15,15 +16,22 @@ const InvestPage: FC = () => {
 
   return (
     <div className={s.investPage}>
-      <div className="">
-        <Container maxWidth='lg'>
-          <h1>Invest</h1>
-          <div className={s.investmentSummary}>
-            <Grid container spacing={0} maxWidth={1600}>
-              <Grid item xs={8}>
-                <h2>Investment Summary</h2>
-                <div className={s.info}>
-                  <div className={s.infoMain}>
+      <div className={s.container}>
+        <div className=''>
+          <h2>Invest</h2>
+          <div className={s.main}>
+            <div className={s.info}>
+              <div className={s.infoMain}>
+                <h3 className={s.investmentSummaryTitle}>Investment Summary</h3>
+                <div className={s.assetInfo}>
+                  <div className={s.imageWrap}>
+                    <img
+                      src={deal.images[0]?.src}
+                      width={180}
+                      className={s.image}
+                    />
+                  </div>
+                  <div>
                     <h3 className={s.assetTitle}>{deal.title}</h3>
                     <div className={s.addressLine1}>
                       {deal.line1} {deal.line2}
@@ -31,32 +39,54 @@ const InvestPage: FC = () => {
                     <div className={s.addressLine2}>
                       {deal.city}, {deal.state} {deal.postalCode} {deal.country}
                     </div>
-                    <div className={s.imageWrap}>
-                      <img
-                        src={deal.images[0]?.src}
-                        width={300}
-                        className={s.image}
-                      />
-                    </div>
-                  </div>
-                  <div className={s.controls}>
-                    <div className={s.tokenQuantityTitle}>Token Quantity</div>
-                    <div className={s.tokenQuantityInputWrap}>
-                      <button type="button" className={s.tokenQuantityButton}>-</button>
-                      <input type="number" className={s.tokenQuantityInput} />
-                      <button type="button" className={s.tokenQuantityButton}>+</button>
-                    </div>
-                    <div>{deal.tokenPrice} / Token</div>
-                    <div>{deal.tokensLeft} available</div>
                   </div>
                 </div>
-              </Grid>
-              <Grid item xs={3}>
-                Order Summary
-              </Grid>
-            </Grid>
+              </div>
+              <div className={s.controls}>
+                <div className={s.tokenQuantityTitle}>Token Quantity</div>
+                <div className={s.tokenQuantityInputWrap}>
+                  <button
+                    type='button'
+                    className={classNames(s.tokenQuantityButton, s.tokenQuantityButtonRemove)}
+                    onClick={() => setTokenQuantity(quantity => quantity - 1)}
+                  >
+                    -
+                  </button>
+                  <input
+                    type='text'
+                    className={s.tokenQuantityInput}
+                    value={tokenQuantity}
+                    onChange={(e) => {
+                      const value = parseInt(e.target.value);
+                      setTokenQuantity(isNaN(value) ? undefined : value);
+                    }}
+                  />
+                  <button
+                    type='button'
+                    className={classNames(s.tokenQuantityButton, s.tokenQuantityButtonAdd)}
+                    onClick={() => setTokenQuantity(quantity => quantity + 1)}
+                  >
+                    +
+                  </button>
+                </div>
+                <div>{deal.tokenPrice} / Token</div>
+                <div>{deal.tokensLeft} available</div>
+              </div>
+            </div>
+            <div className={s.orderSummary}>
+              <h3 className={s.orderSummaryTitle}>Order Summary</h3>
+              <div className={s.orderTotal}>
+                <span className={s.orderTotalTitle}>Order Total</span>
+                <span className={s.orderTotalValue}>$50</span>
+              </div>
+              <div className={s.submitWrap}>
+                <Button className={s.submitButton} variant='contained'>
+                  Continue to Payment
+                </Button>
+              </div>
+            </div>
           </div>
-        </Container>
+        </div>
       </div>
     </div>
   );
