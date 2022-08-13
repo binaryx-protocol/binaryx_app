758bc9cf14c325ab380f8687f693ef0ce6b8b88e
Dima Goncharenko <dimeeentor@gmail.com>
2022-08-13 09:28:21 +0200

    Added navigation menu
    Navigation menu for desktop and mobile

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
+import Navigation from 'components/navigation';
+import s from '../app/components/navigation/styles.module.scss';
+import MenuElement from 'components/pages/account_page/AccountMenu/MenuElement';
 
 const Home: NextPage<{ data: string }> = (props) => {
   const { data } = props;
 
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => setShowMenu(!showMenu);
+
   return (
     <div>
+      <Navigation>
+        <div className={s.headerWrapper}>
+          <img src="#" alt="company_logo" className={s.logo} />
+          <nav className={`${s.navLinks} ${showMenu ? '' : s.hidden} `}>
+            <MenuElement link={'#'} body={'Marketplace'} />
+            <MenuElement link={'#'} body={'About us'} />
+            <MenuElement link={'#'} body={'Learn'} />
+            <MenuElement link={'#'} body={'Blog'} />
+            <MenuElement link={'#'} body={'Sell my property'} />
+            <MenuElement link={'/account'} body={'Account'} />
+            <MenuElement
+              link={'#'}
+              body={'View Marketplace'}
+              className={s.viewMarketplace}
+            />
+          </nav>
+          <div className={s.burgerMenuField} onClick={toggleMenu}>
+            <div className={s.burgerShowMenu}></div>
+          </div>
+        </div>
+      </Navigation>
+
       <h1>Hello from NextJS! - Home</h1>
       {data}
     </div>
