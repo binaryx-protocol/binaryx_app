cb7796ae7b2cc2cc2c7cfcf64153f8773dac9777
Dmitriy Green <dmitriy.zelenyi96@gmail.com>
2022-09-03 10:05:32 +0300

    Style updates

--- src/client/app/components/pages/account_page/AccountMenu/index.tsx
+++ src/client/app/components/pages/account_page/AccountMenu/index.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactChild } from 'react';
-import OrderBlock from '../OrderBlock';
 import s from './style.module.scss';
+import Link from 'next/link';
 
 type Props = {
   children: ReactChild | ReactChild[];
@@ -9,11 +9,15 @@ type Props = {
 const AccountMenu: FC<Props> = (props: Props) => {
   return (
     <div className={s.sidebarMenu}>
-      <img
-        src="https://binaryxestate.s3.eu-central-1.amazonaws.com/images/common/logo_black_horizontal.svg"
-        alt="logo"
-        className={s.logo}
-      />
+      <Link href="/marketplace">
+        <a className={s.logoLink}>
+          <img
+            src="https://binaryxestate.s3.eu-central-1.amazonaws.com/images/common/logo_black_horizontal.svg"
+            alt="logo"
+            className={s.logo}
+          />
+        </a>
+      </Link>
       {props.children}
     </div>
   );
