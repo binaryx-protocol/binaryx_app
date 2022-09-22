2459201f977f1a80aab7b4ef6d45d72a14ae3dc9
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-09-22 16:08:51 +0300

    #wallet_poc: connect raw mm, get accounts

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -34,7 +34,51 @@ const MyApp: FC<Props> = ({ Component, pageProps }) => {
     }
   }, []);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const main = async () => {
+        const ethereum = window.ethereum
+        console.log('window.ethereum', ethereum)
+        console.log('ethereum.isConnected()', ethereum.isConnected())
+        ethereum.on('accountsChanged', (accounts) => {
+          // Handle the new accounts, or lack thereof.
+          // "accounts" will always be an array, but it can be empty.
+          console.log('accountsChanged', accounts)
+          // window.location.reload();
+        });
 
+        ethereum.on('chainChanged', (chainId) => {
+          // Handle the new chain.
+          // Correctly handling chain changes can be complicated.
+          // We recommend reloading the page unless you have good reason not to.
+          console.log('chainId', chainId)
+          // window.location.reload();
+        });
+
+        ethereum
+            .request({ method: 'eth_accounts' })
+            .then((accounts) => {
+              console.log('eth_accounts', accounts)
+            })
+
+        ethereum
+            .request({ method: 'eth_requestAccounts' })
+            .then((accounts) => {
+              console.log('eth_requestAccounts', accounts)
+            })
+            .catch((err) => {
+              if (err.code === 4001) {
+                // EIP-1193 userRejectedRequest error
+                // If this happens, the user rejected the connection request.
+                console.log('Please connect to MetaMask.');
+              } else {
+                console.error(err);
+              }
+            });
+      };
+      main()
+    }
+  }, []);
 
   return (
     <ApolloProvider client={client}>
