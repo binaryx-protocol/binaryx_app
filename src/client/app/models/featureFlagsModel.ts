45fa2eb0fd43c2affee3b499148db4160aff10c9
Dmitriy Green <dmitriy.zelenyi96@gmail.com>
2022-09-30 14:01:11 +0800

    Add ability to set FF through URL

--- src/client/app/models/featureFlagsModel.ts
+++ src/client/app/models/featureFlagsModel.ts
@@ -1,13 +1,24 @@
 import { atom } from 'jotai'
+import getUrlParams from '../utils/getUrlParams';
 
 type FeatureFlags = {
-    FF_MM: boolean
+    FF_MM: boolean;
+    FF_LP_PARALLAX: boolean;
 }
 
 const localStorage = typeof window !== 'undefined'
     ? window.localStorage
     : { getItem(){} }
 
+function getFlagFromUrl(flagName: string) {
+    return getUrlParams().get(flagName);
+}
+
+function getFlag(flagName: string) {
+    return getFlagFromUrl(flagName) || localStorage.getItem(flagName);
+}
+
 export const $featureFlags = atom<FeatureFlags>({
-    FF_MM: localStorage.getItem('FF_MM') === 'true'
+    FF_MM: getFlag('FF_MM') === 'true',
+    FF_LP_PARALLAX: getFlag('FF_LP_PARALLAX') === 'true'
 })
