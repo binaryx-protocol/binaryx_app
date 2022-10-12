7158fbb4348309e054ab5e6bf5a1f6d645c5e028
Dmitriy Green <dmitriy.zelenyi96@gmail.com>
2022-10-12 22:32:08 +0800

    Fix popup error

--- src/client/pages/home-page/components/PopupMenu/index.tsx
+++ src/client/pages/home-page/components/PopupMenu/index.tsx
@@ -1,12 +1,6 @@
 // @ts-nocheck
 
-import {
-  Dispatch,
-  KeyboardEvent,
-  SetStateAction,
-  useEffect,
-  useRef,
-} from 'react';
+import { KeyboardEvent, useEffect, useRef } from 'react';
 import NavSocialImage from '../NavSocialImage';
 import s from './styles.module.scss';
 import IconDiscord from '../NavSocialImage/IconDiscord';
@@ -16,7 +10,7 @@ import IconTelegram from '../NavSocialImage/IconTelegram';
 
 type Props = {
   isShowing: boolean;
-  setIsShowing: Dispatch<SetStateAction<boolean>>;
+  setIsShowing: any;
   handleFormSubmit: (event: any) => void;
 };
 
@@ -36,72 +30,92 @@ const PopupMenu = ({ isShowing, setIsShowing, handleFormSubmit }: Props) => {
     popupMenu.current?.focus();
   }, []);
 
+  if (!isShowing) {
+    return null;
+  }
+
   return (
-    isShowing && (
-      <div
-        tabIndex={0}
-        ref={popupMenu}
-        className={s.popupMenu}
-        onKeyUp={onKeyUp}
-      >
-        <div className={s.popupMenuBgOverlay} onClick={handleOnClick} />
-        <div className={s.container}>
-          <div className={s.title}>
-            <h2 style={{ color: 'var(--font-color_blue-light)' }}>
-              Join our social media
-            </h2>
-            <h2>To be a part of our community</h2>
-          </div>
-          <div className={s.socialLinks}>
-            <NavSocialImage
-              link={'https://discord.gg/kJqgYh7G9G'}
-              icon={<IconDiscord className={s.socialImage} width={isMobile ? 36 : 55} height={isMobile ? 26 : 40} fill="#335367" />}
-            />
-            <NavSocialImage
-              link={'https://twitter.com/realBinaryx'}
-              icon={<IconTwitter className={s.socialImage} width={isMobile ? 34 : 45} height={isMobile ? 25 : 41} fill="#335367" />}
-            />
-            <NavSocialImage
-              link={'https://www.linkedin.com/company/realbinaryx/'}
-              icon={<IconLinkedIn className={s.socialImage} width={isMobile ? 32 : 45} height={isMobile ? 29 : 44} fill="#335367" />}
-            />
-            <NavSocialImage
-              link={'https://t.me/binaryxnews'}
-              icon={<IconTelegram className={s.socialImage} width={isMobile ? 30 : 43} height={isMobile ? 30 : 43} fill="#335367" />}
-            />
-          </div>
-          <span>or</span>
-          <h3 className={s.appeal}>
-            Join waitlist and receive only important news via email. No spam, we
-            promise
-          </h3>
-          <form className={s.formSection} onSubmit={handleFormSubmit}>
-            <input
-              className={s.inputName}
-              type="text"
-              placeholder="Your name:"
-            />
-            <input
-              className={s.inputEmail}
-              type="email"
-              placeholder="Your email:"
-            />
-            <button className={s.btnToSubscribe} type="submit">
-              Subscribe
-            </button>
-            <p className={s.privacyPolicy}>
-              By subscribing, you agree to the Privacy Policy and Terms of
-              Service
-            </p>
-          </form>
-          <img
-            className={s.closePopup}
-            src={'https://cdn-icons-png.flaticon.com/512/1828/1828778.png'}
-            onClick={handleOnClick}
+    <div tabIndex={0} ref={popupMenu} className={s.popupMenu} onKeyUp={onKeyUp}>
+      <div className={s.popupMenuBgOverlay} onClick={handleOnClick} />
+      <div className={s.container}>
+        <div className={s.title}>
+          <h2 style={{ color: 'var(--font-color_blue-light)' }}>
+            Join our social media
+          </h2>
+          <h2>To be a part of our community</h2>
+        </div>
+        <div className={s.socialLinks}>
+          <NavSocialImage
+            link={'https://discord.gg/kJqgYh7G9G'}
+            icon={
+              <IconDiscord
+                className={s.socialImage}
+                width={isMobile ? 36 : 55}
+                height={isMobile ? 26 : 40}
+                fill="#335367"
+              />
+            }
+          />
+          <NavSocialImage
+            link={'https://twitter.com/realBinaryx'}
+            icon={
+              <IconTwitter
+                className={s.socialImage}
+                width={isMobile ? 34 : 45}
+                height={isMobile ? 25 : 41}
+                fill="#335367"
+              />
+            }
+          />
+          <NavSocialImage
+            link={'https://www.linkedin.com/company/realbinaryx/'}
+            icon={
+              <IconLinkedIn
+                className={s.socialImage}
+                width={isMobile ? 32 : 45}
+                height={isMobile ? 29 : 44}
+                fill="#335367"
+              />
+            }
+          />
+          <NavSocialImage
+            link={'https://t.me/binaryxnews'}
+            icon={
+              <IconTelegram
+                className={s.socialImage}
+                width={isMobile ? 30 : 43}
+                height={isMobile ? 30 : 43}
+                fill="#335367"
+              />
+            }
           />
         </div>
+        <span>or</span>
+        <h3 className={s.appeal}>
+          Join waitlist and receive only important news via email. No spam, we
+          promise
+        </h3>
+        <form className={s.formSection} onSubmit={handleFormSubmit}>
+          <input className={s.inputName} type="text" placeholder="Your name:" />
+          <input
+            className={s.inputEmail}
+            type="email"
+            placeholder="Your email:"
+          />
+          <button className={s.btnToSubscribe} type="submit">
+            Subscribe
+          </button>
+          <p className={s.privacyPolicy}>
+            By subscribing, you agree to the Privacy Policy and Terms of Service
+          </p>
+        </form>
+        <img
+          className={s.closePopup}
+          src={'https://cdn-icons-png.flaticon.com/512/1828/1828778.png'}
+          onClick={handleOnClick}
+        />
       </div>
-    )
+    </div>
   );
 };
 
