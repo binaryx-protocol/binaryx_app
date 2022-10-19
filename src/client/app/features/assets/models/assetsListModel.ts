115d955fc962dd25ff3083bcf8ca49fdca705c5b
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-10-19 18:01:01 +0300

    #invest: fix types

--- src/client/app/features/assets/models/assetsListModel.ts
+++ src/client/app/features/assets/models/assetsListModel.ts
@@ -1,46 +1,9 @@
 import { atom } from 'jotai'
 import * as metaMaskModel from "../../../models/metaMaskModel";
 import {loadable} from "jotai/utils";
-import {ethers} from "ethers";
-import {assetsManagerAbi} from "./abis";
 import * as rpcConfigModel from "../../../models/rpcConfigModel";
-import {RpcConfig} from "../../../models/rpcConfigModel";
-import {AssetAddress, BcAsset, AssetStatuses} from "../types";
+import { AssetStatuses} from "../types";
 import {arbClient} from "./arbClient";
-
-const assetAddressAttrs  = (): AssetAddress => ({
-  country: 'UA',
-  state: 'Che',
-  city: 'Cherkassy',
-  postalCode: '19600',
-  addressLine1: 'Khreschatik 1',
-  addressLine2: '5th floor',
-})
-
-const defaultAttrs = (): BcAsset => ({
-  name: 'Name',
-  symbol: 'SYM',
-  title: 'Title ' + Math.random(),
-  description: 'Description is a long story to tell you about the asset. Let\'s write it another time.',
-  status: AssetStatuses.upcoming,
-  originalOwner: 'REPLACE_ME',
-  legalDocuments: ['https://google.com', 'https://mit.com'],
-  // propertyAddress: assetAddressAttrs(),
-})
-
-export const $doCreateAsset = atom(null, async (get) => {
-  const $walletReadiness = get(metaMaskModel.$walletReadiness)
-  const $rpcConfig = get(rpcConfigModel.$rpcConfig)
-
-  if ($walletReadiness === 'ready' && $rpcConfig) {
-    await arbClient.createAsset(
-      $rpcConfig,
-      defaultAttrs()
-    );
-  } else {
-    throw new Error("Wallet is not connected or config is not provided");
-  }
-})
 
 export const $doActivate = atom(null, async (get, set, args: { id: number }) => {
   const $walletReadiness = get(metaMaskModel.$walletReadiness)
