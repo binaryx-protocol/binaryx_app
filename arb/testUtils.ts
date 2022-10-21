e5de6a5d816fccac6bd4cbac14895a6b1eaf8165
Dmitriy Green <dmitriy.zelenyi96@gmail.com>
2022-10-21 18:27:32 +0800

    Fix types

--- arb/testUtils.ts
+++ arb/testUtils.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai";
 
-export const bnToInt = (bn) => {
+export const bnToInt = (bn: any) => {
   return parseInt(bn.toString())
 }
 
-export const expectBn = (arg) => {
+export const expectBn = (arg: any) => {
   return expect(bnToInt(arg))
 }
