27d9b5df21a74a92339e8b57f4178724890fe5c5
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-10-29 21:24:13 +0300

    #demo1: header view & connect wallet

--- src/client/app/shared/header/views/DesktopBar.tsx
+++ src/client/app/shared/header/views/DesktopBar.tsx
@@ -1,18 +1,62 @@
 import s from "./DesktopBar.module.scss";
 import {Button} from "../../ui/views/Button";
+import {paths} from "../../../../../../pkg/paths";
+import Link from "next/link";
 
-export const DesktopBar = () => {
+type Props = {
+  account: string
+  hasAddress: boolean
+  onWalletConnect: () => void
+}
+
+export const DesktopBar = ({ account, onWalletConnect, hasAddress }: Props) => {
   return (
     <div className={s.root}>
       <div className={s.container}>
-        <img
-          src="https://binaryxestate.s3.eu-central-1.amazonaws.com/images/common/logo_black_horizontal.svg"
-          alt="company_logo"
-          className={s.logo}
-        />
-        <div>123</div>
-        <div>
-          <Button>Connect Wallet</Button>
+        <Link href={paths.home()} passHref>
+          <a className={s.logo}>
+            <img
+              src="https://binaryxestate.s3.eu-central-1.amazonaws.com/images/common/logo_black_horizontal.svg"
+              alt="Binaryx"
+            />
+          </a>
+        </Link>
+        <div className={s.nav}>
+          <Link href={paths.home()} passHref>
+            <a>
+              Home
+            </a>
+          </Link>
+          <Link href={paths.home()} passHref>
+            <a>
+              Marketplace
+            </a>
+          </Link>
+          <Link href={paths.home()} passHref>
+            <a>
+              Oracles
+            </a>
+          </Link>
+          <Link href={paths.home()} passHref>
+            <a>
+              Governance
+            </a>
+          </Link>
+        </div>
+        <div className={s.buttons}>
+          <Link href={paths.home()}>
+            <Button color="light">
+              List Property
+            </Button>
+          </Link>
+          {
+            hasAddress
+              ? <span className={s.accountAddress}>{account}</span>
+              :
+              <Button onClick={onWalletConnect}>
+                Connect Wallet
+              </Button>
+          }
         </div>
       </div>
     </div>
