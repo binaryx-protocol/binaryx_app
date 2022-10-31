5e4013929b7671a08514c805b9b7d75dcd2bb1ac
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-11-01 00:31:44 +0200

    #demo2: require wallet on invest page

--- src/client/app/features/assets/controllers/InvestController.tsx
+++ src/client/app/features/assets/controllers/InvestController.tsx
@@ -1,7 +1,11 @@
 import {InvestAmountSection} from "../views/InvestAmountSection";
+import {useAtomValue} from "jotai";
+import * as metaMaskModel from "../../../core/models/metaMaskModel";
 
 export const InvestController = () => {
-  return <InvestAmountSection />
+  const isAccountConnected = useAtomValue(metaMaskModel.$isAccountConnected)
+
+  return isAccountConnected ? <InvestAmountSection /> : <div style={{ padding: '10px', textAlign: 'center' }}>Loading</div>
 }
 
 const T = {
