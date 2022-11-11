dd5d8b8dc2206f68e2e7a13347e1e82a8b2e9d1b
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-11-11 10:48:21 +0200

    #poc_v3: using loadFixture for tests

--- contracts_arbitrum/tests/v3/SeriesMaster.ts
+++ contracts_arbitrum/tests/v3/SeriesMaster.ts
@@ -2,34 +2,48 @@ import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import hre, {ethers, upgrades, web3} from "hardhat";
 
-const usdtDecimals = 1e6;
-const usdtInitialBalance = 1000;
-const ONE_YEAR_IN_SECS = 31536000;
-
 describe("SeriesMaster", function () {
   async function deployFixture() {
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, wallet2] = await ethers.getSigners();
 
-    const UsdtfToken = await ethers.getContractFactory("UsdtfToken");
-    const usdtfToken = await UsdtfToken.deploy(web3.utils.toBN(usdtInitialBalance).mul(web3.utils.toBN(usdtDecimals)).toString());
+    const SeriesMaster = await ethers.getContractFactory("SeriesMaster");
+    const seriesMaster = await upgrades.deployProxy(SeriesMaster, [[], 'https://otoco.io/dashpanel/entity/']);
 
-    const Class = await ethers.getContractFactory("AssetsToken");
-    const sc = await Class.deploy(usdtfToken.address);
-
-    return { sc, owner, otherAccount, usdtfToken };
+    return { seriesMaster, owner, wallet2 };
   }
 
   describe("Deployment", function () {
     it("Initialize Master", async function () {
-      const [owner, wallet2, wallet3, wallet4] = await ethers.getSigners();
-
-      const SeriesMaster = await ethers.getContractFactory("SeriesMaster");
-      const seriesMaster = await upgrades.deployProxy(SeriesMaster, [[], 'https://otoco.io/dashpanel/entity/']);
-      await seriesMaster.deployed();
+      const { seriesMaster, owner } = await loadFixture(deployFixture);
 
       expect(await seriesMaster.name()).to.equal("Binaryx Series");
       expect(await seriesMaster.symbol()).to.equal("BNRXS");
       expect(await seriesMaster.owner()).to.equal(owner.address);
     });
+    // it("Creating series with correct fees and wrong fees", async function () {
+    //
+    //   const gasPrice = ethers.BigNumber.from("2000000000");
+    //   const gasLimit = ethers.BigNumber.from("150000");
+    //   const otocoBaseFee = await otocoMaster.baseFee();
+    //
+    //   // 34750 reduction from gas limit is what is spended before check happens
+    //   const amountToPayForSpinUp = ethers.BigNumber.from(gasPrice).mul(gasLimit.sub(34750)).div(otocoBaseFee);
+    //   // Remove 1% from the correct amount needed
+    //   const notEnoughToPayForSpinUp = amountToPayForSpinUp.mul(100).div(101);
+    //
+    //   // Try to create without the proper amount of ETH Value, expect to fail
+    //   await expect(otocoMaster.createSeries(2, owner.address, "New Entity", {gasPrice, gasLimit, value:notEnoughToPayForSpinUp}))
+    //     .to.be.revertedWith('OtoCoMaster: Not enough ETH paid for the execution.');
+    //
+    //   // Expected to successfully create a new entity
+    //   const transaction = await otocoMaster.createSeries(2, owner.address, "New Entity", {gasPrice, gasLimit, value:amountToPayForSpinUp});
+    //   await expect(transaction).to.emit(otocoMaster, 'Transfer').withArgs(zeroAddress(), owner.address, 7);
+    //   expect((await otocoMaster.series(7)).jurisdiction).to.be.equal(2)
+    //   expect((await otocoMaster.series(7)).name).to.be.equal("New Entity - Series 5")
+    //
+    //   // Chech if the amount to pay was transferred
+    //   expect(await ethers.provider.getBalance(otocoMaster.address)).to.be.equal(amountToPayForSpinUp);
+    //
+    // });
   });
 });
