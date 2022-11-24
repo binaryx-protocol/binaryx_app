6d160827a44a6c4999607b5b4af7f4828800c6d5
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2022-11-24 18:22:55 +0200

    #bnrx_root_token: upd token name

--- contracts_arbitrum/contracts/v3/BNRXToken.sol
+++ contracts_arbitrum/contracts/v3/BNRXToken.sol
@@ -7,9 +7,10 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
+// icon https://binaryx-media-i1.s3.eu-west-1.amazonaws.com/binaryx_token_icon.svg
 contract BNRXToken is Initializable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable {
   function initialize(address owner) initializer public {
-    __ERC20_init("Binaryx", "BNRX");
+    __ERC20_init("Binaryx Protocol", "BNRX");
     __ERC20Burnable_init();
     __Pausable_init();
     __Ownable_init();
