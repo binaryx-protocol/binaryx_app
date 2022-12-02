bc0b886935fc120e633f3199f5eb356d9037a0c1
Space-zee <andrii.filonenko@yozh.io>
2022-12-02 13:08:52 +0200

    feat:next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   experimental: {
     newNextLinkBehavior: false,
   },
+  eslint: {
+    // to enable eslint checks
+    // npm install --save-dev eslint-config-next
+    // more info https://nextjs.org/docs/basic-features/eslint
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
